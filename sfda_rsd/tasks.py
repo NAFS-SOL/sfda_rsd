85c1cdeb18003828c513821832c6865ed0c8c045
SajidEycon <sajid.eycon@gmail.com>
2026-04-16 08:47:00 +0200

    Fix - module import error for sfda_rsd.connectors

--- sfda_rsd/tasks.py
+++ sfda_rsd/tasks.py
@@ -11,7 +11,7 @@ def retry_failed_notifications():
 	_retry()
 
 
-def sync_drug_list():
+def sync_drug_list(): 
 	"""Daily: sync SFDA drug list to local database."""
 	from sfda_rsd.connectors.services.query_service import sync_drug_list as _sync
 	_sync()
